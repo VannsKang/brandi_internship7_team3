3ea5d7f98beec0e8a2d9f12cc7fffc918e99a405
Gyuwon Seong <gw777seong@gmail.com>
2020-12-08 02:04:16 +0900

    [관리자_상품관리 > 상품관리]

--- Backend/service/product_service.py
+++ Backend/service/product_service.py
@@ -1,10 +1,10 @@
 from utils.exceptions import NotExistError
 
+
 class ProductService:
     def __init__(self, product_dao, config):
         self.product_dao = product_dao
         self.config = config
-
 
     def get_products(self, filter_data, conn):
         if filter_data.get('start_date') and filter_data.get('end_date'):
@@ -24,40 +24,40 @@ class ProductService:
 
     # NOTE soomyung's API get color data
     def get_colors(self, conn):
-        
-        colors =  self.product_dao.get_colors(conn)
-        
+
+        colors = self.product_dao.get_colors(conn)
+
         return colors
-    
+
     # NOTE soomyung's API get size data
     def get_sizes(self, conn):
-        
-        sizes =  self.product_dao.get_sizes(conn)
-        
+
+        sizes = self.product_dao.get_sizes(conn)
+
         return sizes
-    
+
     # NOTE soomyung's API get first_categories data
     def get_first_categories(self, conn):
-        
-        first_categories =  self.product_dao.get_first_categories(conn)
-        
+
+        first_categories = self.product_dao.get_first_categories(conn)
+
         return first_categories
 
     # NOTE soomyung's API get second_categories data
     def get_second_categories(self, first_category_id, conn):
-        
+
         if "first_category_id" not in first_category_id:
             raise KeyError
-        
-        second_categories =  self.product_dao.get_second_categories(first_category_id, conn)
-        
+
+        second_categories = self.product_dao.get_second_categories(first_category_id, conn)
+
         return second_categories
-    
+
     # NOTE soomyung's API post new products data
-    def add_new_product(self,product_info, conn):
+    def add_new_product(self, product_info, conn):
         # print(product_info)
         # NOTE number, product_option_number => custom auto increments
-        
-        new_product =  self.product_dao.add_new_product(product_info,conn)
+
+        new_product = self.product_dao.add_new_product(product_info, conn)
 
         return new_product
